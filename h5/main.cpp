be09112fbc70b2c47466d001362f303bb673fd35
Juan Yu <sigrace0417@gmail.com>
2025-12-03 14:52:09 +0200

    pointer and ref

---
+++ h5/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "classa1.h"
+#include "classa2.h"
+
+
+using namespace std;
+
+int main()
+{
+    int a=5;
+    cout << "a:n arvo on " <<a<<" ja osoite on "<<&a << endl;
+
+    int *pointerA=&a;
+    cout << "Pointterin osoittama osoite on: " <<pointerA<<endl;
+    cout<<"Pointterin osoittaman muistipaikan arvo on: "<<*pointerA<<endl;
+
+    int &refA=a;
+    cout<<"refA osoittaa osoitteeseen on: "<<&refA<<endl;
+    cout<<"refA:n osoittaman muistipaikan arvo on: "<<refA<<endl;
+
+    int b=6;
+    cout<<&b<<endl;
+    /*refA=b;
+    cout<<"refA uusi osoittaa osoitteeseen on: "<<&refA<<endl;
+    cout<<"refA:n uusi osoittaman muistipaikan arvo on: "<<refA<<endl;
+    cout<<"refA:n uusi osoittaman muistipaikan arvo on: "<<a<<endl;*/
+    pointerA=&b;
+
+    //*pointerA=b;
+    cout << "Pointterin uusi osoittama osoite on: " <<pointerA<<endl;
+    cout<<"Pointterin uusi osoittaman muistipaikan arvo on: "<<*pointerA<<endl;
+
+    ClassB objB;
+    objB.setInfo("Olion B asettama info");
+
+    ClassA1 objA1(objB);
+    objA1.setBinfo("Olion objA1 asettama info");
+
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA1: "<<objA1.getBinfo()<<endl;
+
+    ClassB &refB=objB;
+    ClassA2 objA2(refB);
+    objA2.setBinfo("Olion Agr asettama info");
+    cout<<"objB: "<<objB.getInfo()<<endl;
+    cout<<"objA2 "<<objA2.getBinfo()<<endl;
+    cout<<endl;
+
+    return 0;
+}
